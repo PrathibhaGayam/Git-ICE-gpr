cee8baa5d880b52bb278c6e0c7fc2b936f9f4536
Student <s525719@ch140004m.nwmissouri.edu>
2016-11-14 16:44:47 -0600

    Added SeaVC. Renamed first and second view controllers as EhVC and BeeVC

--- Git-ICE-gpr/BeeVC.swift
+++ Git-ICE-gpr/BeeVC.swift
@@ -1,5 +1,5 @@
 //
-//  FirstViewController.swift
+//  SecondViewController.swift
 //  Git-ICE-gpr
 //
 //  Created by Gayam,Prathibha on 11/14/16.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class BeeVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
