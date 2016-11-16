024a41731cbe2fdee94d9d1fd82514c6b834bdd9
Student <s525770@ch140005m.nwmissouri.edu>
2016-11-16 16:23:28 -0600

    WROTE A PRINT STATEMENT IN EHEE VC

--- Git-ICE-gpr/EhVC.swift
+++ Git-ICE-gpr/EhVC.swift
@@ -13,6 +13,7 @@ class EhVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("help B")
     }
 
     override func didReceiveMemoryWarning() {
